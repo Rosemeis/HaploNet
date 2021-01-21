6db90cd95249c5c3e507ade8b48e5a2d2818b6a2
Rosemeis <meisnerucph@gmail.com>
2021-01-21 11:32:14 +0100

    Added option for unphased genotypes

--- generateNPYfromVCF.py
+++ generateNPYfromVCF.py
@@ -1,19 +1,34 @@
 """
-HaploNet. Generate .npy file from phased VCF file.
+HaploNet.
+Generate .npy file from VCF file.
 """
 
+__author__ = "Jonas Meisner"
+
+# Libraries
 import numpy as np
 import allel
 import argparse
 
+# Argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-vcf")
-parser.add_argument("-out")
+parser.add_argument("-vcf",
+    help="Input vcf-file of genotypes")
+parser.add_argument("-unphased", action="store_true",
+    help="Toggle for unphased genotype data")
+parser.add_argument("-out",
+    help="Output filepath")
 args = parser.parse_args()
 
 # Load VCF
+print("Loading VCF file and generating NumPy file.")
 vcf = allel.read_vcf(args.vcf)
 G = vcf['calldata/GT']
 
 # Save .npy file in np.int8
-np.save(args.out, G.reshape(G.shape[0], -1))
+if args.unphased:
+    print("Saving unphased genotypes as " + args.out + ".npy")
+    np.save(args.out, np.sum(G, axis=2)//2)
+else:
+    print("Saving phased genotypes as " + args.out + ".npy")
+    np.save(args.out, G.reshape(G.shape[0], -1))
